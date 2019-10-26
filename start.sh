1cca992b3fc6278fb5352c5cb4d456ddb9a45c2d
Yangwook Jeong <wooky92@naver.com>
2019-10-26 15:27:31 +0900

    -

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-# Home
-docker run --rm --name blog -v "C:/dev/record/blog:/srv/jekyll" -p 49160:4000 -it jekyll/jekyll jekyll serve --force_polling --livereload
-
-# Company
-# docker run --rm --name blog -v "C:/Users/Administrator/Desktop/Y/blog:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll jekyll serve --force_polling --livereload
+docker run --rm --name blog -v $(pwd -W):/srv/jekyll -p 49160:4000 -it jekyll/jekyll jekyll serve --force_polling --livereload
